75ad4bec84f4e09ccce80fd97833699e00a02217
YeoheungMinSsi <20161538@office.dongseo.ac.kr>
2024-08-22 18:16:49 +0900

    일부부분 백업입니다.

--- src/App.test.js
+++ src/App.test.js
@@ -1,12 +1,7 @@
 import React, {useState, useEffect} from 'react';
 import { Route, Routes } from 'react-router-dom';
-import Kakao from './Kakaotest';
+import Kakao from './Kakao';
 import './css/index.css'
-import RegionSelector from "./config/RegionSelector";
-import TransactionMethod from "./config/TransactionMethod";
-import {Building} from "./config/Building";
-import BuildingMethod from "./config/BuildingMethod";
-import AreaSet from "./config/AreaSet";
 
 function App() {
 
@@ -15,8 +10,6 @@ function App() {
   const handleClick = () => {
     setSideMenuOpen(!sideMenuOpen);
   }
-
-
 
   return (
       <div id={"App"}>
@@ -46,65 +39,6 @@ function App() {
           <Routes id={"mapContent"}>
             <Route path="/" element={<Kakao />} />
           </Routes>
-          <article>
-            <div id={"searchContainer"}>
-              <input id={"search"} type="text" placeholder={"지역, 지하철역, 회사명"}/>
-            </div>
-
-          </article>
-          <article>
-            <button id={'location'}>
-              <img src={require('./icon/location.png')} alt=""/>
-            </button>
-          </article>
-          <div id={"appSide"} className={sideMenuOpen ? "open" : "closed"}>
-            <article id={"appSideButton"}>
-              {/*슬라이드 인/아웃 버튼*/}
-              <div id={"appSideMoveIcon"}>
-                <img src={sideMenuOpen ? require("./icon/vecterback2.png") : require('./icon/vecter2.png')} alt=""/>
-                <img src={sideMenuOpen ? require("./icon/vecterback2.png") : require('./icon/vecter2.png')} alt=""/>
-              </div>
-              <div id={sideMenuOpen ? "appSideClickOut" : "appSideClickIn"} onClick={handleClick}>
-                <div>
-                  <button>
-                    <img src={sideMenuOpen ? require("./icon/Vectorback.png") : require("./icon/Vector.png")} alt=""/>
-                  </button>
-                </div>
-              </div>
-            </article>
-            <article id={"appSideChoice"}>
-              {/*간편 메뉴 버튼*/}
-              <div className={"appSCMenu"}>
-                <img src={require("./icon/shopping.png")} alt=""/>
-                <div>편의시절</div>
-              </div>
-              <div className={"appSCMenu"}>
-                <img src={require("./icon/sefety.png")} alt=""/>
-                <div>안전시설</div>
-              </div>
-              <div className={"appSCMenu"}>
-                <img src={require("./icon/hospital.png")} alt=""/>
-                <div>의료시설</div>
-              </div>
-              <div className={"appSCMenu"}>
-                <img src={require("./icon/plus.png")} alt=""/>
-                <div>기타시설</div>
-              </div>
-            </article>
-          </div>
-          <article id={"hiddenMenu"} className={sideMenuOpen ? "open" : "closed"}>
-            <div id={'hiddenTable'}>
-              <RegionSelector />
-              <TransactionMethod/>
-              <BuildingMethod/>
-              <AreaSet/>
-              <div className={"selectList"}>
-                <div>n개의 결과<br/><hr/></div>
-                {/*여기도 컨포넌트 or 큰 창이 띄울수 있는 버튼*/}
-                <div> 리스트</div>
-              </div>
-            </div>
-          </article>
         </section>
       </div>
   );
